4714d8a86bf6b59d705bcbce88e26c8ecc127d96
ASiracusa <fedorafilmz@gmail.com>
2017-12-20 11:36:20 -0500

    There ya go

--- dungeoncrawlergame.py
+++ dungeoncrawlergame.py
@@ -1,2 +1,18 @@
-print("test I guess")
-# Can you guys see this now?
+from tkinter import *
+
+class Application(Frame):
+
+    def __init__(self, master):
+        super().__init__(master)
+        self.grid()
+        self.create_widgets()
+
+    def create_widgets(self):
+        Label(self, text="*insert game here*").grid(row=0, column=0, sticky=W)
+
+root = Tk()
+root.title("Dungeon Crawler")
+root.geometry("405x300")
+
+app = Application(root)
+root.mainloop()
