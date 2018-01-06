8882a9d320f90cddb3006e22f102f0b514832dca
ASiracusa <fedorafilmz@gmail.com>
2018-01-06 13:59:39 -0500

    I forgot to commit this a long time ago lol

--- dungeoncrawlergame.py
+++ dungeoncrawlergame.py
@@ -8,19 +8,28 @@ class Application(Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        Label(self, text=":D").grid(row=0, column=0, sticky=W)
-        upb = Button(self, text=" ↑ ")
-        upb.grid(row=1, column=1)
-        leftb = Button(self, text=" ← ")
-        leftb.grid(row=2, column=0)
-        rightb = Button(self, text=" → ")
-        rightb.grid(row=2, column=2)
-        downb = Button(self, text=" ↓ ")
-        downb.grid(row=3, column=1)
+        Canvas(self, bg="#cc7130", width=405, height=315, bd=0, highlightthickness=0).grid(row=0, rowspan=17, column=0, columnspan=4)
+
+        self.playarea = Text(self, width=50, height=13, state=DISABLED)
+        self.playarea.grid(row=0, column=0, columnspan=15)
+
+        self.upb = Button(self, text=" ▲ ", bg="#8B4513", activebackground="#633310")
+        self.upb.grid(row=2, column=1)
+        self.leftb = Button(self, text=" ◀ ", bg="#8B4513", activebackground="#633310")
+        self.leftb.grid(row=3, column=0, sticky=E)
+        self.rightb = Button(self, text=" ▶ ", bg="#8B4513", activebackground="#633310")
+        self.rightb.grid(row=3, column=2, sticky=W)
+        self.downb = Button(self, text=" ▼ ", bg="#8B4513", activebackground="#633310")
+        self.downb.grid(row=4, column=1)
+        self.centerb = Button(self, text=" ▼ ", bg="#8B4513", activebackground="#633310", fg="#8B4513", activeforeground="#633310")
+        self.centerb.grid(row=3, column=1)
+
+        Label(self, text="\t\t\t\t\t\t          ", bg="#cc7130").grid(row=1, column=3, columnspan=11)
 
 root = Tk()
 root.title("GOI")
-root.geometry("900x600")
+root.geometry("404x400")
+root.configure(bg="#cc7130")
 
 app = Application(root)
 root.mainloop()
