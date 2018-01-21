c195f8d1b6a9695e296ca911f0aeb41dc5f43f4a
tevaalon <32883463+tevaalon@users.noreply.github.com>
2018-01-21 13:01:21 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	screen_win.py

---
+++ screen_death.py
@@ -0,0 +1,38 @@
+from tkinter import *
+class DeathScreen(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.grid()
+        self.create_widgets()
+    def create_widgets(self):
+        Canvas(self, bg="#cc7130", width=550, height=300, bd=0, highlightthickness=0).grid(row=0, column=0,columnspan=6, rowspan=6)
+        Label(self,
+              text = "You are dead :(",
+              bg = "#cc7130",
+              fg = "Black",
+              font = ("Fixedsys 40"),
+             ). grid(row=0, column=1, columnspan = 4)
+        Button(self,
+               text = "Play Again",
+               font = ("Fixedsys 16"),
+               bg ="#8B4513",
+               command= quit
+               ).grid(row=2,column=1,sticky=W)
+        Button(self,
+               text="Main Menu",
+               font=("Fixedsys 16"),
+               bg="#8B4513",
+               command=quit
+               ).grid(row=2, column=2, sticky=W)
+        Button(self,
+               text="Exit",
+               font=("Fixedsys 16"),
+               bg="#8B4513",
+               command = exit
+               ).grid(row=2, column=3, sticky=W)
+    def test(self):
+        print(" ")
+root = Tk()
+root.title("Death Screen")
+app = DeathScreen(root)
+root.mainloop()
