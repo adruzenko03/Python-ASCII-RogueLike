7bef2b2ab80ae5b153f5f24cb2be40b133de2154
adruz <adruzenko03@gmail.com>
2018-01-23 14:22:20 -0500

    Make screen loader file

--- screen_win.py
+++ screen_win.py
@@ -6,31 +6,27 @@ class Winscreen(Frame):
         self.create_widgets()
         self.restart = restart
     def create_widgets(self):
+        x = 1
         Canvas(self, bg="#cc7130", width=500, height=300, bd=0, highlightthickness=0).grid(row=0, column=0,columnspan=6, rowspan=6)
         Label(self,
-              text = "You Won!",
+              text = "You beat level"+ str(x),
               bg = "#cc7130",
               fg = "Black",
               font = ("Fixedsys 50"),
              ). grid(row=0, column=1, columnspan = 3)
         Button(self,
-               text = "Play Again",
+               text = "Continue",
                font = ("Fixedsys 16"),
                bg ="#8B4513",
                command= self.restartF
                ).grid(row=2,column=1,sticky=W)
-        Button(self,
-               text="Main Menu",
-               font=("Fixedsys 16"),
-               bg="#8B4513",
-               command=quit
-               ).grid(row=2, column=2, sticky=W)
         Button(self,
                text="Exit",
                font=("Fixedsys 16"),
                bg="#8B4513",
                command = exit
                ).grid(row=2, column=3, sticky=W)
+        x += 1
     def restartF(self):
         self.restart()
 
